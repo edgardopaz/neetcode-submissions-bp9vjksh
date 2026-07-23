141603ca1907f80b6b570bd200bac926aec5939f
Edgardo Paz-Romero <dedgardo381@gmail.com>
2026-07-23 19:33:22 -0400

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        res = ""
+
+        for c in s:
+            if c.isalnum():
+                res += c.lower()
+        
+        left = 0
+        right = len(res) - 1
+
+        while left <= right:
+            if res[left] != res[right]:
+                return False
+            left += 1
+            right -= 1
+
+        return True    
